62f1479260cd2b69460b93c1f9b186acef09bdf5
오영선 <63745627+oyoungsun@users.noreply.github.com>
2024-04-08 16:02:29 +0900

    Title: 코드트리 오마카세, Time: 124ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240408/코드트리 오마카세/codetree-omakase.py
+++ 240408/코드트리 오마카세/codetree-omakase.py
@@ -1,6 +1,6 @@
 def doEat(prev, now):
     global l, sushi, people, time
-    for t in range(prev+1, now):
+    for t in range(prev+1, now+1):
         if t in time : 
             for comman in time[t] : 
                 name, x= comman[2], comman[1]
@@ -14,19 +14,19 @@ def doEat(prev, now):
                             sushi[name][(l+x-(t%5)) % 5]+= 1
                 else : 
                     n = comman[3]
-                    people[name] = [(l+x-(t%5)) % 5, n]
+                    people[name] = [x, n]
         # 먹기
         for name, info in people.items() : 
             x = info[0]
-            if x in sushi[name].keys() : 
-                sushi[name][x]-=1
+            sx = info[0] - (t%l)
+            if sx in sushi[name].keys() : 
+                sushi[name][sx]-=1
                 people[name]=[x, info[1]-1]
-
 
 prev=0
 l, q = map(int, input().split())
 people = dict() # 사람당 위치, 먹어야하는 초밥 수
-# 초밥(이름) 당 위치(t=0일때 위치)
+# 초밥(이름) 당 위치(t=0일때 위치): 개수
 sushi = dict()
 time = dict()
 for comman in range(q):
@@ -36,17 +36,11 @@ for comman in range(q):
         if t not in time : 
             time[t] = []
         time[t].append((1, x, name))
-        # if(name not in sushi):
-        #     sushi[name] = dict()
-        #     sushi[name][(l+x-(t%5)) % 5] = 1
-        # else : 
-        #     sushi[name][(l+x-(t%5)) % 5] += 1
     elif(c[0]=="200"): #입장
         t, x, name, n = int(c[1]), int(c[2]), c[3], int(c[4])
         if t not in time : 
             time[t] = []
         time[t].append((2, x, name, n))
-        # people[name]= [x, n]
     else : #사진
         t = int(c[1])
         doEat(prev, t)
@@ -54,7 +48,7 @@ for comman in range(q):
         psum=0
         ssum=0
         for k,v in people.items():
-            if(v != 0 ):
+            if(v[1] > 0 ):
                 psum+=1
         for k, v in sushi.items():
             for key, value in v.items():
